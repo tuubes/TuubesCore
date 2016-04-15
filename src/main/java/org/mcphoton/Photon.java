f4d617b8e69f6b489dd601a00ca4d148b41181f3
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-15 09:55:56 +0200

    Return the registries instance

--- src/main/java/org/mcphoton/Photon.java
+++ src/main/java/org/mcphoton/Photon.java
@@ -5,6 +5,14 @@ import org.mcphoton.block.BlockRegistry;
 import org.mcphoton.command.CommandsRegistry;
 import org.mcphoton.entity.EntityRegistry;
 import org.mcphoton.event.EventsManager;
+import org.mcphoton.impl.Main;
+import org.mcphoton.impl.block.PhotonBlockRegistry;
+import org.mcphoton.impl.command.PhotonCommandsRegistry;
+import org.mcphoton.impl.entity.PhotonEntityRegistry;
+import org.mcphoton.impl.event.PhotonEventsManager;
+import org.mcphoton.impl.item.PhotonItemRegistry;
+import org.mcphoton.impl.plugin.PhotonPluginsManager;
+import org.mcphoton.impl.world.PhotonBiomeRegistry;
 import org.mcphoton.item.ItemRegistry;
 import org.mcphoton.network.PacketsManager;
 import org.mcphoton.plugin.PluginsManager;
@@ -18,40 +26,47 @@ import org.mcphoton.world.BiomeRegistry;
 public final class Photon {
 
 	private static final File MAIN_DIR = new File(System.getProperty("user.dir")), PLUGINS_DIR = new File(MAIN_DIR, "plugins");
+	private static final BlockRegistry BLOCK_REGISTRY = new PhotonBlockRegistry();
+	private static final ItemRegistry ITEM_REGISTRY = new PhotonItemRegistry();
+	private static final EntityRegistry ENTITY_REGISTRY = new PhotonEntityRegistry();
+	private static final BiomeRegistry BIOME_REGISTRY = new PhotonBiomeRegistry();
+	private static final CommandsRegistry CMD_REGISTRY = new PhotonCommandsRegistry();
+	private static final EventsManager EVENTS_MANAGER = new PhotonEventsManager();
+	private static final PluginsManager PLUGINS_MANAGER = new PhotonPluginsManager();
 
 	private Photon() {
 	}
 
 	public static PacketsManager getPacketsManager() {
-		return null;
+		return Main.serverInstance.packetsManager;
 	}
 
 	public static PluginsManager getPluginsManager() {
-		return null;
+		return PLUGINS_MANAGER;
 	}
 
 	public static EventsManager getEventsManager() {
-		return null;
+		return EVENTS_MANAGER;
 	}
 
 	public static EntityRegistry getEntityRegistry() {
-		return null;
+		return ENTITY_REGISTRY;
 	}
 
 	public static CommandsRegistry getCommandsRegistry() {
-		return null;
+		return CMD_REGISTRY;
 	}
 
 	public static BlockRegistry getBlockRegistry() {
-		return null;
+		return BLOCK_REGISTRY;
 	}
 
 	public static ItemRegistry getItemRegistry() {
-		return null;
+		return ITEM_REGISTRY;
 	}
 
 	public static BiomeRegistry getBiomeRegistry() {
-		return null;
+		return BIOME_REGISTRY;
 	}
 
 	public static boolean isClient() {
