5120bffc257989af56a7f2282bc0a5c505aca833
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-22 22:41:37 +0200

    Add a constructor for a single PhotonClient

--- src/main/java/org/mcphoton/impl/network/PacketSending.java
+++ src/main/java/org/mcphoton/impl/network/PacketSending.java
@@ -1,16 +1,22 @@
 package org.mcphoton.impl.network;
 
 import java.util.Collection;
+import java.util.Collections;
 import org.mcphoton.network.Packet;
 
 public final class PacketSending {
-	
+
 	public final Packet packet;
 	public final Collection<PhotonClient> clients;
-	
+
+	public PacketSending(Packet packet, PhotonClient client) {
+		this.packet = packet;
+		this.clients = Collections.singletonList(client);
+	}
+
 	public PacketSending(Packet packet, Collection<PhotonClient> clients) {
 		this.packet = packet;
 		this.clients = clients;
 	}
-	
+
 }
