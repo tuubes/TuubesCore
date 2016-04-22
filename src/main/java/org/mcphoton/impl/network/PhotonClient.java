e40d42870c35f111481b6ca3721144842e666219
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-22 17:08:15 +0200

    Simplify PhotonClient.java
    Makes fields accessible from the package and removes accessor and mutator methods.
    The codecs are now stored in a final array, so they must be defined at the client's creation and may never be changed.

--- src/main/java/org/mcphoton/impl/network/PhotonClient.java
+++ src/main/java/org/mcphoton/impl/network/PhotonClient.java
@@ -2,12 +2,7 @@ package org.mcphoton.impl.network;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.ListIterator;
 import java.util.Optional;
 import org.mcphoton.entity.living.player.Player;
 import org.mcphoton.network.Client;
@@ -15,128 +10,47 @@ import org.mcphoton.network.ConnectionState;
 
 /**
  * Implementation of the Client interface.
- * 
+ *
  * @author TheElectronWill
- * 		
+ *
  */
 public final class PhotonClient implements Client {
-	
-	/**
-	 * The codecs, in the output order (server -> client).
-	 */
-	private final List<Codec> codecs = Collections.synchronizedList(new ArrayList<>(2));
-	private final InetSocketAddress address;
-	private final SocketChannel channel;
-	private final MessageReader messageReader;
-	private final MessageWriter messageWriter;
-	private volatile Optional<Player> player = Optional.empty();
-	private volatile int authVerifyToken = -1;
-	private volatile ConnectionState state = ConnectionState.INIT;
-	
-	public PhotonClient(SocketChannel channel) throws IOException {
+
+	final InetSocketAddress address;
+	final Codec[] codecs;//the codecs, in the output order (server -> client).
+	final SocketChannel channel;
+	final MessageReader messageReader;
+	final MessageWriter messageWriter;
+	volatile Optional<Player> player = Optional.empty();
+	volatile int authVerifyToken = -1;
+	volatile ConnectionState state = ConnectionState.INIT;
+
+	public PhotonClient(SocketChannel channel, Codec[] codecs) throws IOException {
 		this.channel = channel;
-		this.messageReader = new MessageReader(channel);
+		this.codecs = codecs;
+		this.messageReader = new MessageReader(channel, 256, 512);
 		this.messageWriter = new MessageWriter(channel);
 		this.address = (InetSocketAddress) channel.getRemoteAddress();
 	}
-	
-	public PhotonClient(SocketChannel channel, MessageReader messageReader, MessageWriter messageWriter) throws IOException {
-		this.channel = channel;
-		this.messageReader = messageReader;
-		this.messageWriter = messageWriter;
-		this.address = (InetSocketAddress) channel.getRemoteAddress();
-	}
-	
+
 	@Override
 	public InetSocketAddress getAddress() {
 		return address;
 	}
-	
+
 	@Override
 	public Optional<Player> getPlayer() {
 		return player;
 	}
-	
+
 	@Override
 	public ConnectionState getConnectionState() {
 		return state;
 	}
-	
+
 	@Override
-	public void setConnectionState(ConnectionState newState) {
-		this.state = newState;
+	public void setConnectionState(ConnectionState state) {
+		this.state = state;
 	}
-	
-	public int getAuthVerifyToken() {
-		return authVerifyToken;
-	}
-	
-	public void setAuthVerifyToken(int authVerifyToken) {
-		this.authVerifyToken = authVerifyToken;
-	}
-	
-	/**
-	 * Gets the codecs, in the output/encoding order (server -> client). The returned list is thread-safe.
-	 */
-	public List<Codec> getCodecs() {
-		return codecs;
-	}
-	
-	public MessageReader getMessageReader() {
-		return messageReader;
-	}
-	
-	public MessageWriter getMessageWriter() {
-		return messageWriter;
-	}
-	
-	/**
-	 * Gets the SocketChannel connected to this client.
-	 */
-	public SocketChannel getChannel() {
-		return channel;
-	}
-	
-	/**
-	 * Decodes a message with the current codecs.
-	 * 
-	 * @param data the message's data
-	 * @return a ByteBuffer that contains the decoded message's data, ie a varInt for the packet's id followed by the
-	 *         packet's data.
-	 */
-	public ByteBuffer decodeWithCodecs(ByteBuffer data) throws IOException {
-		synchronized (codecs) {
-			ListIterator<Codec> it = codecs.listIterator(codecs.size());
-			try {
-				while (it.hasPrevious()) {// loop in reverse order of the list
-					Codec codec = it.previous();
-					data = codec.decode(data);
-				}
-			} catch (Exception ex) {
-				throw new IOException("An error occured while decoding message", ex);
-			}
-		}
-		return data;
-	}
-	
-	/**
-	 * Encodes a message with the current codecs.
-	 * 
-	 * @param data a ByteBuffer that contains the message's data, ie a varInt for the packet's id followed by the
-	 *        packet's data.
-	 * @return the encoded message's data
-	 */
-	public ByteBuffer encodeWithCodecs(ByteBuffer data) throws IOException {
-		synchronized (codecs) {
-			try {
-				for (Codec codec : codecs) {
-					data = codec.encode(data);
-				}
-			} catch (Exception ex) {
-				throw new IOException("An error occured while encoding message", ex);
-			}
-		}
-		return data;
-	}
-	
+
 }
