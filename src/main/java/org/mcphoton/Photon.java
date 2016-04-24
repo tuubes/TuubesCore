31cf146f4d09f4da4d01781a405afc596f3171d2
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-04-24 22:39:52 +0200

    Update

--- src/main/java/org/mcphoton/Photon.java
+++ src/main/java/org/mcphoton/Photon.java
@@ -10,12 +10,12 @@ import org.mcphoton.impl.command.PhotonCommandsRegistry;
 import org.mcphoton.impl.entity.PhotonEntityRegistry;
 import org.mcphoton.impl.event.PhotonEventsManager;
 import org.mcphoton.impl.item.PhotonItemRegistry;
+import org.mcphoton.impl.plugin.JavaPluginLoader;
 import org.mcphoton.impl.plugin.PhotonPluginsManager;
 import org.mcphoton.impl.server.Main;
 import org.mcphoton.impl.world.PhotonBiomeRegistry;
 import org.mcphoton.item.ItemRegistry;
 import org.mcphoton.network.PacketsManager;
-import org.mcphoton.plugin.PluginsManager;
 import org.mcphoton.world.BiomeRegistry;
 
 /**
@@ -25,14 +25,14 @@ import org.mcphoton.world.BiomeRegistry;
  */
 public final class Photon {
 
-	private static final File MAIN_DIR = new File(System.getProperty("user.dir")), PLUGINS_DIR = new File(MAIN_DIR, "plugins");
+	public static final File MAIN_DIR = new File(System.getProperty("user.dir")), PLUGINS_DIR = new File(MAIN_DIR, "plugins"), WORLD_DIR = new File(MAIN_DIR, "worlds");
 	private static final BlockRegistry BLOCK_REGISTRY = new PhotonBlockRegistry();
 	private static final ItemRegistry ITEM_REGISTRY = new PhotonItemRegistry();
 	private static final EntityRegistry ENTITY_REGISTRY = new PhotonEntityRegistry();
 	private static final BiomeRegistry BIOME_REGISTRY = new PhotonBiomeRegistry();
 	private static final CommandsRegistry CMD_REGISTRY = new PhotonCommandsRegistry();
 	private static final EventsManager EVENTS_MANAGER = new PhotonEventsManager();
-	private static final PluginsManager PLUGINS_MANAGER = new PhotonPluginsManager();
+	private static final PhotonPluginsManager PLUGINS_MANAGER = new PhotonPluginsManager(new JavaPluginLoader());
 
 	private Photon() {
 	}
@@ -41,7 +41,7 @@ public final class Photon {
 		return Main.serverInstance.packetsManager;
 	}
 
-	public static PluginsManager getPluginsManager() {
+	public static PhotonPluginsManager getPluginsManager() {
 		return PLUGINS_MANAGER;
 	}
 
@@ -82,7 +82,7 @@ public final class Photon {
 	}
 
 	public static String getMinecraftVersion() {
-		return "1.9";
+		return "1.9.2";
 	}
 
 	public static File getMainDirectory() {
