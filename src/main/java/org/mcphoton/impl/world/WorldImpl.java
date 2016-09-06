3ca5133fbb49bea6f63274bbb6388b2af2df1cbd
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-09-06 16:35:42 +0200

    Update WorldImpl

--- src/main/java/org/mcphoton/impl/world/WorldImpl.java
+++ src/main/java/org/mcphoton/impl/world/WorldImpl.java
@@ -18,24 +18,30 @@
  */
 package org.mcphoton.impl.world;
 
+import com.electronwill.utils.Bag;
+import com.electronwill.utils.IndexMap;
 import com.electronwill.utils.SimpleBag;
 import java.io.File;
 import java.util.Collection;
 import org.mcphoton.Photon;
 import org.mcphoton.command.WorldCommandRegistry;
+import org.mcphoton.entity.Entity;
 import org.mcphoton.entity.living.Player;
 import org.mcphoton.event.WorldEventsManager;
 import org.mcphoton.impl.command.WorldCommandRegistryImpl;
 import org.mcphoton.impl.event.WorldEventsManagerImpl;
 import org.mcphoton.impl.plugin.WorldPluginsManagerImpl;
 import org.mcphoton.plugin.WorldPluginsManager;
-import org.mcphoton.world.Location;
+import org.mcphoton.utils.ImmutableLocation;
+import org.mcphoton.utils.Location;
+import org.mcphoton.utils.MutableLocation;
+import org.mcphoton.world.ChunkGenerator;
 import org.mcphoton.world.World;
 import org.mcphoton.world.WorldType;
 import org.mcphoton.world.protection.WorldAccessManager;
 
 /**
- * Basic implementation of World.
+ * Basic implementation of World. It is thread-safe.
  *
  * @author TheElectronWill
  */
@@ -47,14 +53,68 @@ public class WorldImpl implements World {
 
 	protected final WorldType type;
 	protected final Collection<Player> players = new SimpleBag<>();
+	protected final IndexMap<Entity> entities = new IndexMap<>();// ids of the world's entities.
+	protected final Bag<Integer> removedIds = new SimpleBag(100, 50);// ids of the removed entities. Reusing them avoids fragmentation.
 
 	protected final WorldPluginsManager pluginsManager = new WorldPluginsManagerImpl(this);
 	protected final WorldEventsManager eventsManager = new WorldEventsManagerImpl();
 	protected final WorldCommandRegistry commandRegistry = new WorldCommandRegistryImpl();
+	protected volatile ChunkGenerator chunkGenerator;
+	protected volatile WorldAccessManager accessManager;
 
 	public WorldImpl(String name, WorldType type) {
 		this.name = name;
 		this.type = type;
+	}
+
+	@Override
+	public ChunkGenerator getChunkGenerator() {
+		return chunkGenerator;
+	}
+
+	@Override
+	public void setChunkGenerator(ChunkGenerator generator) {
+		this.chunkGenerator = generator;
+	}
+
+	@Override
+	public Entity getEntity(int entityId) {
+		synchronized (entities) {
+			return entities.get(entityId);
+		}
+	}
+
+	@Override
+	public void spawnEntity(Entity entity, double x, double y, double z) {
+		synchronized (entities) {
+			int nextId;
+			if (removedIds.isEmpty()) {
+				nextId = entities.size();// need a new id
+			} else {
+				nextId = removedIds.get(0);// reuse this id
+				removedIds.remove(0);
+			}
+			entity.initEntityId(nextId);
+			entities.put(nextId, entity);
+		}
+		MutableLocation loc = (MutableLocation) entity.getLocation();
+		loc.setX(x);
+		loc.setY(y);
+		loc.setZ(z);
+		//TODO spawn packet
+	}
+
+	@Override
+	public void removeEntity(Entity entity) {
+		removeEntity(entity.getEntityId());
+	}
+
+	@Override
+	public void removeEntity(int entityId) {
+		synchronized (entities) {
+			entities.remove(entityId);
+			removedIds.add(entityId);
+		}
 	}
 
 	@Override
@@ -82,21 +142,14 @@ public class WorldImpl implements World {
 
 	@Override
 	public Location getSpawn() {
-		return new Location(spawnX, spawnY, spawnZ, this);
+		return new ImmutableLocation(spawnX, spawnY, spawnZ, this);
 	}
 
 	@Override
-	public synchronized void setSpawn(int x, int y, int z) {
+	public synchronized void setSpawn(double x, double y, double z) {
 		this.spawnX = x;
 		this.spawnY = y;
 		this.spawnZ = z;
-	}
-
-	@Override
-	public synchronized void setSpawn(Location spawn) {
-		this.spawnX = spawn.getX();
-		this.spawnY = spawn.getY();
-		this.spawnZ = spawn.getZ();
 	}
 
 	@Override
@@ -106,7 +159,7 @@ public class WorldImpl implements World {
 
 	@Override
 	public void save() {
-		; //TODO
+		//TODO
 	}
 
 	@Override
@@ -116,12 +169,12 @@ public class WorldImpl implements World {
 
 	@Override
 	public WorldAccessManager getAccessManager() {
-		return null;//TODO
+		return accessManager;
 	}
 
 	@Override
 	public void setAccessManager(WorldAccessManager manager) {
-		; //TODO
+		this.accessManager = manager;
 	}
 
 	@Override
