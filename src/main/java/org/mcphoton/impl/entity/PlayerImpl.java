2e177cdce9b0652b92650a435041ba842204aaf1
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-09-11 18:24:30 +0200

    Work on PlayerImpl

--- src/main/java/org/mcphoton/impl/entity/PlayerImpl.java
+++ src/main/java/org/mcphoton/impl/entity/PlayerImpl.java
@@ -19,24 +19,68 @@
 package org.mcphoton.impl.entity;
 
 import java.util.UUID;
+import org.mcphoton.entity.EntityType;
+import org.mcphoton.entity.MetadataWriter;
+import org.mcphoton.entity.living.AbstractLivingEntity;
 import org.mcphoton.entity.living.Player;
 import org.mcphoton.inventory.Inventory;
-import org.mcphoton.world.Location;
+import org.mcphoton.messaging.ChatMessage;
+import org.mcphoton.network.Packet;
+import org.mcphoton.network.play.clientbound.SpawnPlayerPacket;
+import org.mcphoton.utils.Location;
 
 /**
  *
  * @author TheElectronWill
  */
-public class PlayerImpl implements Player {
+public class PlayerImpl extends AbstractLivingEntity implements Player {
 
-	private final String name;
 	private final UUID accoundId;
-	private volatile Location location;
+	private final String name;
+	private volatile String nameInChat, nameInList;
 
-	public PlayerImpl(String name, UUID accoundId, Location location) {
+	public PlayerImpl(String name, UUID accoundId) {
 		this.name = name;
 		this.accoundId = accoundId;
-		this.location = location;
+		this.nameInChat = name;
+	}
+
+	@Override
+	public Packet constructSpawnPacket() {
+		return new SpawnPlayerPacket(this);
+	}
+	@Override
+	public UUID getAccountId() {
+		return accoundId;
+	}
+	@Override
+	public Location getCompassTarget() {
+		; //TODO
+	}
+	@Override
+	public void setCompassTarget(Location target) {
+		; //TODO
+	}
+
+	@Override
+	public Inventory getEnderChest() {
+		; //TODO
+	}
+
+	@Override
+	public Inventory getInventory() {
+		; //TODO
+	}
+
+	@Override
+	public void setLocation(Location l) {
+		; //TODO
+	}
+
+
+	@Override
+	public float getMaxHealth() {
+		return 20f;
 	}
 
 	@Override
@@ -44,71 +88,115 @@ public class PlayerImpl implements Player {
 		return name;
 	}
 
+
 	@Override
-	public UUID getAccountId() {
-		return accoundId;
+	public String getNameInChat() {
+		return nameInChat;
 	}
 
 	@Override
-	public Location getLocation() {
-		return location;
+	public void setNameInChat(String name) {
+		this.nameInChat = name;
 	}
 
 	@Override
-	public void setLocation(Location l) {
-		this.location = l;
+	public String getNameInPlayerList() {
+		return nameInList;
 	}
 
 	@Override
-	public void setBanned(boolean b) {
+	public void setNameInPlayerList(String name) {
+		this.nameInList = name;
 	}
-
 	@Override
-	public void setOp(boolean b) {
+	public EntityType getType() {
+		; //TODO
 	}
-
 	@Override
-	public void setWhitelisted(boolean b) {
+	public boolean hasPermission(String permission) {
+		return true;//TODO
 	}
-
-	@Override
-	public boolean isBanned() {
-		return false;
-	}
-
 	@Override
 	public boolean hasPlayedBefore() {
-		return false;
+		return false;//TODO
 	}
-
+	@Override
+	public boolean isBanned() {
+		return false; //TODO
+	}
+	@Override
+	public void setBanned(boolean b) {
+		; //TODO
+	}
 	@Override
 	public boolean isOnline() {
-		return true;
+		; //TODO
 	}
 
 	@Override
 	public boolean isOp() {
-		return false;
+		return false;//TODO
 	}
+
+	@Override
+	public void setOp(boolean b) {
+		; //TODO
+	}
+
 
 	@Override
 	public boolean isWhitelisted() {
-		return false;
+		; //TODO
 	}
 
 	@Override
-	public Inventory getInventory() {
-		return null;
+	public void setWhitelisted(boolean b) {
+		; //TODO
+	}
+
+
+	@Override
+	public void kickPlayer(String message) {
+		; //TODO
 	}
 
 	@Override
-	public boolean hasPermission(String permission) {
-		return false;
+	public void leaveVehicle() {
+		; //TODO
+	}
+
+	@Override
+	public void sendMessage(CharSequence msg) {
+		; //TODO
+	}
+
+	@Override
+	public void sendMessage(ChatMessage msg) {
+		; //TODO
+	}
+
+	@Override
+	public void sendRawMessage(String rawMessage) {
+		; //TODO
+	}
+
+	@Override
+	public boolean teleport(Location location) {
+		; //TODO
 	}
 
 	@Override
 	public String toString() {
-		return "PhotonPlayer{" + "name=" + name + ", accoundId=" + accoundId + ", location=" + location + '}';
+		return "PhotonPlayer{" + "name=" + name + ", accoundId=" + accoundId + '}';
+	}
+
+	@Override
+	public void writeMetadata(MetadataWriter writer) {
+		super.writeMetadata(writer);
+		//TODO 11: additional hearts
+		//TODO 12: score
+		//TODO 13: skins parts
+		writer.writeByte(14, 1);//main hand = right, TODO choose
 	}
 
 }
