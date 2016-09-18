7786fc3c7566fa939d998aecaa3e881d5e8d9176
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-09-18 19:37:15 +0200

    Remove class path from photon's loggers

--- src/main/java/org/slf4j/impl/PhotonLoggerFactory.java
+++ src/main/java/org/slf4j/impl/PhotonLoggerFactory.java
@@ -18,6 +18,9 @@
  */
 package org.slf4j.impl;
 
+import com.electronwill.utils.StringUtils;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import org.slf4j.ILoggerFactory;
@@ -26,10 +29,16 @@ import org.slf4j.Logger;
 public class PhotonLoggerFactory implements ILoggerFactory {
 
 	private final ConcurrentMap<String, Logger> loggerMap = new ConcurrentHashMap<>();
-
+	
 	@Override
 	public Logger getLogger(String name) {
-		return loggerMap.computeIfAbsent(name, (k) -> new PhotonLogger(name));
+		if(name.startsWith("org.mcphoton")){
+			List<String> parts = new ArrayList<>();
+			StringUtils.split(name, '.', parts);
+			name = parts.get(parts.size()-1);//uses the last part
+		}
+		final String loggerName = name;//final String for the lambda expression
+		return loggerMap.computeIfAbsent(name, (k) -> new PhotonLogger(loggerName));
 	}
 
 }
