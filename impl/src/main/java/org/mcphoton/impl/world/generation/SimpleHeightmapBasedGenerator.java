2fc179ca41cc59b9a2a4f1c6954a672d7a0654d0
TheElectronWill <theelectronwill@gmail.com>
2017-05-24 10:36:40 +0200

    Fix heightmap-based chunk generator

--- impl/src/main/java/org/mcphoton/impl/world/generation/SimpleHeightmapBasedGenerator.java
+++ impl/src/main/java/org/mcphoton/impl/world/generation/SimpleHeightmapBasedGenerator.java
@@ -1,5 +1,6 @@
 package org.mcphoton.impl.world.generation;
 
+import com.github.steveice10.opennbt.tag.builtin.CompoundTag;
 import java.util.Random;
 import org.mcphoton.impl.world.ChunkColumnImpl;
 import org.mcphoton.impl.world.ChunkSectionImpl;
@@ -9,14 +10,14 @@ import org.mcphoton.world.ChunkSection;
 import org.mcphoton.world.World;
 
 /**
- *
  * @author TheElectronWill
  */
 public class SimpleHeightmapBasedGenerator implements ChunkGenerator {
 
 	private static final double DEFAULT_FACTOR = 1.0 / 40.0;
 	private static final int DEFAULT_MIN = 50, DEFAULT_MAX = 200, DEFAULT_SEA_LEVEL = 70;
-	private static final int ID_BEDROCK = 7 << 4, ID_WATER = 9 << 4, ID_STONE = 1 << 4, ID_GRASS = 2 << 4, ID_SAND = 12 << 4;
+	private static final int ID_BEDROCK = 7 << 4, ID_WATER = 9 << 4, ID_STONE = 1 << 4,
+							 ID_GRASS = 2 << 4, ID_SAND = 12	<< 4;
 	private static final byte BIOME_PLAINS = 1, BIOME_OCEAN = 0;
 
 	private final World world;
@@ -24,7 +25,8 @@ public class SimpleHeightmapBasedGenerator implements ChunkGenerator {
 	private final double factor;
 	private final int minValue, maxValue, seaLevel;
 
-	public SimpleHeightmapBasedGenerator(World world, Random random, double factor, int minValue, int maxValue, int seaLevel) {
+	public SimpleHeightmapBasedGenerator(World world, Random random, double factor, int minValue,
+										 int maxValue, int seaLevel) {
 		this.world = world;
 		this.noise = new SimplexNoise(random);
 		this.factor = factor;
@@ -46,15 +48,16 @@ public class SimpleHeightmapBasedGenerator implements ChunkGenerator {
 		for (int x = startBlockX; x < startBlockX + 16; x++) {
 			for (int z = startBlockZ; z < startBlockZ + 16; z++) {
 				double noiseValue = noise.generate(x * factor, z * factor);
-				int height = (int) ((noiseValue + 1.0) / 2.0) * (maxValue - minValue) + minValue;
+				int height = (int)((noiseValue + 1.0) / 2.0) * (maxValue - minValue) + minValue;
 				if (height > maxHeight) {
 					maxHeight = height;
 				}
+				heightmap[x][z] = height;
 			}
 		}
-		ChunkSection[] sections = new ChunkSection[(int) Math.ceil(maxHeight / 16.0)];
+		ChunkSectionImpl[] sections = new ChunkSectionImpl[16];
 		for (int i = 0; i < sections.length; i++) {
-			sections[i] = new ChunkSectionImpl(13);
+			sections[i] = new ChunkSectionImpl(true);
 		}
 		// Bedrock layer
 		ChunkSection zeroth = sections[0];
@@ -82,6 +85,7 @@ public class SimpleHeightmapBasedGenerator implements ChunkGenerator {
 				}
 			}
 		}
-		return new ChunkColumnImpl(biomesData, sections);
+		return new ChunkColumnImpl(startBlockX / 16, startBlockZ / 16, sections, biomesData,
+								   new CompoundTag[0]);
 	}
 }
