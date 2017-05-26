8217fc2b48798cc5cebddf2be02656857131655b
TheElectronWill <theelectronwill@gmail.com>
2017-05-26 23:00:01 +0200

    Implement AbstractMob, LivingMob and PlayerImpl metadata

--- impl/src/main/java/org/mcphoton/impl/entity/mobs/AbstractMob.java
+++ impl/src/main/java/org/mcphoton/impl/entity/mobs/AbstractMob.java
@@ -1,6 +1,13 @@
 package org.mcphoton.impl.entity.mobs;
 
+import com.github.steveice10.mc.protocol.data.game.entity.metadata.EmptyMetadataStorage;
+import com.github.steveice10.mc.protocol.data.game.entity.metadata.MetadataType;
+import com.github.steveice10.mc.protocol.data.game.entity.metadata.TrackedMetadataStorage;
+import com.github.steveice10.mc.protocol.data.game.entity.metadata.TrackedMetadataValue;
 import com.github.steveice10.mc.protocol.packet.ingame.server.entity.spawn.ServerSpawnMobPacket;
+import com.github.steveice10.packetlib.packet.Packet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import org.mcphoton.impl.entity.AbstractEntity;
 import org.mcphoton.utils.Location;
@@ -9,8 +16,23 @@ import org.mcphoton.utils.Location;
  * @author TheElectronWill
  */
 public abstract class AbstractMob extends AbstractEntity {
+	/** Rotation angle, in radians (converted when writing packets). */
+	protected volatile float pitch, yaw, headPitch;
+	protected final TrackedMetadataStorage dataStorage = new TrackedMetadataStorage(initializeDataStorage());
+
 	protected AbstractMob(Location location) {
 		super(location);
+	}
+
+	protected List<TrackedMetadataValue> initializeDataStorage() {
+		List<TrackedMetadataValue> values = new ArrayList<>();
+		values.add(new TrackedMetadataValue(MetadataType.BYTE, 0));//status byte
+		values.add(new TrackedMetadataValue(MetadataType.VARINT, 300));//air level
+		values.add(new TrackedMetadataValue(MetadataType.STRING, ""));//custom name
+		values.add(new TrackedMetadataValue(MetadataType.BOOLEAN, false));//is custom name visible
+		values.add(new TrackedMetadataValue(MetadataType.BOOLEAN, false));//is silent
+		values.add(new TrackedMetadataValue(MetadataType.BOOLEAN, false));//true if no gravity
+		return values;
 	}
 
 	@Override
@@ -19,8 +41,10 @@ public abstract class AbstractMob extends AbstractEntity {
 	@Override
 	public void spawn() {
 		super.spawn();
+		int id = this.id;
 		UUID uuid = new UUID(0, id);
 		int typeId = getType().getId();
-		//TODO new ServerSpawnMobPacket(id, uuid, typeId,position,yaw,pitch,headYaw,velocity,metadata);
+		Packet packet = new ServerSpawnMobPacket(id, uuid, typeId, position, yaw, pitch, headPitch,
+												 velocity, EmptyMetadataStorage.INSTANCE);
 	}
 }
--- impl/src/main/java/org/mcphoton/impl/entity/mobs/PlayerImpl.java
+++ impl/src/main/java/org/mcphoton/impl/entity/mobs/PlayerImpl.java
@@ -1,6 +1,9 @@
 package org.mcphoton.impl.entity.mobs;
 
+import com.github.steveice10.mc.protocol.data.game.entity.metadata.MetadataType;
+import com.github.steveice10.mc.protocol.data.game.entity.metadata.TrackedMetadataValue;
 import com.github.steveice10.mc.protocol.packet.ingame.server.entity.spawn.ServerSpawnPlayerPacket;
+import java.util.List;
 import java.util.UUID;
 import org.mcphoton.entity.EntityType;
 import org.mcphoton.entity.living.Player;
@@ -18,19 +21,29 @@ public class PlayerImpl extends AbstractMob implements Player {
 
 	public PlayerImpl(Location location, String name, UUID accoundId) {
 		super(location);
-	    this.name = name;
+		this.name = name;
 		this.accoundId = accoundId;
 	}
 
-    @Override
-    public void spawn() {
-        world.addEntity(this);
-        UUID uuid = accoundId;
-        int typeId = getType().getId();
-        //TODO new ServerSpawnPlayerPacket(...)
-    }
+	@Override
+	protected List<TrackedMetadataValue> initializeDataStorage() {
+		List<TrackedMetadataValue> values = super.initializeDataStorage();
+		values.add(new TrackedMetadataValue(MetadataType.FLOAT, 0.0));//additional hearts
+		values.add(new TrackedMetadataValue(MetadataType.VARINT, 0));//player score
+		values.add(new TrackedMetadataValue(MetadataType.BYTE, 0));//skin settings
+		values.add(new TrackedMetadataValue(MetadataType.BYTE, 1));//main hand, 1/0: right/left
+		return values;
+	}
 
-    @Override
+	@Override
+	public void spawn() {
+		world.addEntity(this);
+		UUID uuid = accoundId;
+		int typeId = getType().getId();
+		//TODO new ServerSpawnPlayerPacket(...)
+	}
+
+	@Override
 	public Player asPlayer() {
 		return this;
 	}
@@ -40,45 +53,45 @@ public class PlayerImpl extends AbstractMob implements Player {
 		return accoundId;
 	}
 
-    @Override
-    public Inventory getInventory() {
-        throw new UnsupportedOperationException("Not implemented yet.");
-    }
+	@Override
+	public Inventory getInventory() {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
 
-    @Override
-    public void sendMessage(CharSequence msg) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+	@Override
+	public void sendMessage(CharSequence msg) {
+		throw new UnsupportedOperationException("Not implemented yet.");
 
-    }
+	}
 
-    @Override
-    public void sendMessage(ChatMessage msg) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+	@Override
+	public void sendMessage(ChatMessage msg) {
+		throw new UnsupportedOperationException("Not implemented yet.");
 
-    }
+	}
 
-    @Override
-    public boolean hasPermission(String permission, boolean ifNotSet) {
-        throw new UnsupportedOperationException("Not implemented yet.");
-    }
+	@Override
+	public boolean hasPermission(String permission, boolean ifNotSet) {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
 
-    @Override
-    public AbstractMobType getType() {
-        return null;//TODO StandardMobs.PLAYER
-    }
+	@Override
+	public AbstractMobType getType() {
+		return null;//TODO StandardMobs.PLAYER
+	}
 
-    @Override
-    public String getName() {
-        return name;
-    }
+	@Override
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public boolean isPermissionSet(String permission) {
-        throw new UnsupportedOperationException("Not implemented yet.");
-    }
+	@Override
+	public boolean isPermissionSet(String permission) {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
 
-    @Override
-    public void setLocation(Location l) {
-	    teleport(l);
-    }
+	@Override
+	public void setLocation(Location l) {
+		teleport(l);
+	}
 }
