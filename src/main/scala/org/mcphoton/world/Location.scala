2d3098a02556316f01c1385e9f046c9011c9e25f
TheElectronWill <theelectronwill@gmail.com>
2017-09-12 19:11:20 +0200

    Location: add toString and fromString

--- src/main/scala/org/mcphoton/world/Location.scala
+++ src/main/scala/org/mcphoton/world/Location.scala
@@ -1,5 +1,6 @@
 package org.mcphoton.world
 
+import com.electronwill.utils.StringUtils._
 import com.electronwill.utils.{Vec3d, Vec3i}
 
 /**
@@ -44,6 +45,8 @@ private final class Location3d(val coords: Vec3d, override val world: World) ext
 	override def toVec3d: Vec3d = coords
 
 	override def toVec3i: Vec3i = coords
+
+	override def toString: String = s"$x,$y,$z,${world.getName}"
 }
 
 /**
@@ -65,16 +68,14 @@ private final class Location3i(val coords: Vec3i, override val world: World) ext
 	override def toVec3d: Vec3d = coords
 
 	override def toVec3i: Vec3i = coords
+
+	override def toString: String = s"$blockX,$blockY,$blockZ,${world.getName}"
 }
 
 object Location {
 	/* Creates a new Location from a Vec3d and a World */
 	def apply(coords: Vec3d, world: World): Location = {
 		new Location3d(coords, world)
-	}
-
-	private[block] def test() = {
-
 	}
 
 	/* Creates a new Location from a Vec3i and a World */
@@ -97,4 +98,18 @@ object Location {
 
 	/* Converts a Location to a Vec3d */
 	implicit def toVec3i(location: Location): Vec3i = location.toVec3i
+
+	def fromString(s: String): Location = {
+		val splitted = split(s, ',')
+		val s0 = splitted.get(0)
+		val s1 = splitted.get(1)
+		val s2 = splitted.get(2)
+		val s3 = splitted.get(3)
+		val sCoords = s0 + s1 + s2
+		if (contains(sCoords, '.', 'e', 'E')) {
+			new Location3d(Vec3d.fromString(sCoords), WorldManager.get(s3).get)
+		} else {
+			new Location3i(Vec3i.fromString(sCoords), WorldManager.get(s3).get)
+		}
+	}
 }
