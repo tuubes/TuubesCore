77fb404b05cf64c967a49cbe38f03b0e68b844cd
TheElectronWill <theelectronwill@gmail.com>
2017-11-18 23:17:53 +0100

    Move RayBoxIntersection to a separate file

--- src/main/scala/org/mcphoton/world/collision/BoundingBox.scala
+++ src/main/scala/org/mcphoton/world/collision/BoundingBox.scala
@@ -146,19 +146,4 @@ final class BoundingBox(val lower: Vec3d, val upper: Vec3d) {
 		}
 		Some(new RayBoxIntersection(lambda1, lambda2, face1, face2, point, dir))
 	}
-
-	final class RayBoxIntersection(val lambda1: Double, val lambda2: Double,
-								   val face1: Option[BlockFace], val face2: Option[BlockFace],
-								   val rayPoint: Vec3d, val rayDir: Vec3d) {
-		def firstIntersectionPoint: Vec3d = {
-			rayDir * lambda1 + rayPoint
-		}
-
-		def secondIntersectionPoint: Vec3d = {
-			rayDir * lambda2 + rayPoint
-		}
-		def intersectionVector: Vec3d = {
-			rayDir * (lambda2 - lambda1)
-		}
-	}
 }
