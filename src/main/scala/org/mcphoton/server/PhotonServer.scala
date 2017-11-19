247d15ed2cabc0c841cf19f4d2bd25e25862b2b2
TheElectronWill <theelectronwill@gmail.com>
2017-11-19 17:11:54 +0100

    Rudimentary plugins loading

--- src/main/scala/org/mcphoton/server/PhotonServer.scala
+++ src/main/scala/org/mcphoton/server/PhotonServer.scala
@@ -6,9 +6,11 @@ import org.mcphoton.GameRegistry
 import org.mcphoton.command.{CommandSystem, StopCommand}
 import org.mcphoton.entity.mobs.Player
 import org.mcphoton.network.ProtocolLibAdapter
+import org.mcphoton.plugin._
 import org.mcphoton.world.{World, WorldType}
 
 import scala.collection.mutable
+import scala.util.{Failure, Success}
 
 /**
  * @author TheElectronWill
@@ -16,12 +18,14 @@ import scala.collection.mutable
 object PhotonServer extends StrictLogging {
 	// Constant infos
 	final val Version: String = "0.5-alpha"
+	final val MinecraftVersion: String = "1.12"
 
 	// Directories
 	final val DirMain = File(System.getProperty("user.dir"))
 	final val DirConfig: File = DirMain / "config"
 	final val DirPlugins: File = DirMain / "plugins"
 	final val DirWorlds: File = DirMain / "worlds"
+	final val DirLogs: File = DirMain / "logs"
 	final val Config: ServerConfig = new ServerConfig
 
 	// Worlds
@@ -60,7 +64,8 @@ object PhotonServer extends StrictLogging {
 		logger.info("Loading the Photon's commands")
 		loadCommands()
 
-		//TODO load plugins
+		logger.info("Loading the plugins")
+		loadPlugins()
 
 		logger.info("Starting the TCP server")
 		startTcp()
@@ -69,7 +74,7 @@ object PhotonServer extends StrictLogging {
 	}
 
 	private def loadDirs(): Unit = {
-		Seq(DirMain, DirConfig, DirPlugins, DirWorlds)
+		Seq(DirMain, DirConfig, DirPlugins, DirWorlds, DirLogs)
 		.foreach(_.createIfNotExists(asDirectory = true))
 	}
 
@@ -82,10 +87,64 @@ object PhotonServer extends StrictLogging {
 		for (dir <- DirWorlds.list if dir.isDirectory) {
 			val world = new World(dir.name, WorldType.OVERWORLD)
 			registerWorld(world)
+			//TODO load world config and spawn region
 		}
 	}
 
 	private def loadCommands(): Unit = {
 		CommandSystem.global.register(new StopCommand)
+	}
+
+	/** Loads the plugins that are in DirPlugins. Must be called after loadWorlds(). */
+	private def loadPlugins(): Unit = {
+		val graph = new DependencyGraph()
+		for (file <- DirPlugins.children) {
+			logger.debug(s"Analysing ${file.name}")
+			val inspection = PluginInfos.inspect(file)
+			inspection match {
+				case Success(infos) =>
+					logger.debug(s"Got infos $infos")
+					graph.register(infos)
+				case Failure(t) =>
+					logger.debug(s"Got failure $t")
+					logger.error(s"Error while loading $file", t)
+			}
+		}
+		logger.debug("Building the dependencies graph")
+		graph.build()
+		logger.debug("Resolving the dependencies")
+		val solution = graph.resolve()
+		val nbError = solution.errors.size
+		if (nbError > 0) {
+			logger.warn(s"$nbError out of ${solution.resolvedItems.size} items couldn't be " +
+				s"resolved. See below.")
+			solution.errors.foreach(s => logger.warn("    " + s))
+		}
+		for (resolved <- solution.resolvedItems) {
+			val pluginClass = resolved.loader.loadClass(resolved.infos.pluginClassName)
+			if (classOf[GlobalPlugin].isAssignableFrom(pluginClass)) {
+				val pluginInstance = pluginClass.newInstance().asInstanceOf[GlobalPlugin]
+				GlobalPluginSystem.enable(pluginInstance)
+				Config.spawnLocation.world.pluginSystem.enable(pluginInstance)
+			} else if (classOf[WorldPlugin].isAssignableFrom(pluginClass)) {
+				val pluginInstance = pluginClass.newInstance().asInstanceOf[WorldPlugin]
+				Config.spawnLocation.world.pluginSystem.enable(pluginInstance)
+			} else {
+				logger.warn(s"Plugin $pluginClass is not a GlobalPlugin nor a WorldPlugin.")
+			}
+			//TODO per-world plugin list
+		}
+	}
+
+	private[mcphoton] def shutdown(): Unit = {
+		// TODO
+		logger.warn("Shutdown is not properly implemented yet!")
+		logger.info("Saving the worlds")
+		for (world <- worldsNameMap.valuesIterator) {
+			//TODO world.save()
+		}
+		logger.info("Stopping the TCP server")
+		protocolLibAdapter.stop()
+		logger.info("Shutdown.")
 	}
 }
