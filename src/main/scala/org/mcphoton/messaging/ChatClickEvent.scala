8b3c29fa27f611e506817296d2bae70336b0c6aa
TheElectronWill <theelectronwill@gmail.com>
2017-11-30 14:53:42 +0100

    Use case classes for chat events + move outside of abstract class body

--- src/main/scala/org/mcphoton/messaging/ChatClickEvent.scala
+++ src/main/scala/org/mcphoton/messaging/ChatClickEvent.scala
@@ -3,9 +3,8 @@ package org.mcphoton.messaging
 /**
  * @author TheElectronWill
  */
-abstract sealed class ChatClickEvent(a: String, v: String) extends ChatEvent(a, v) {
-	final class OpenUrl(url: String) extends ChatClickEvent("open_url", url)
-	final class Say(msg: String) extends ChatClickEvent("run_command", msg)
-	final class Suggest(msg: String) extends ChatClickEvent("suggest_command", msg)
-	final class ChangePage(nPage: Int) extends ChatClickEvent("change_page", nPage.toString)
-}
+abstract sealed class ChatClickEvent(a: String, v: String) extends ChatEvent(a, v) {}
+final case class OpenUrl(url: String) extends ChatClickEvent("open_url", url)
+final case class Say(msg: String) extends ChatClickEvent("run_command", msg)
+final case class Suggest(msg: String) extends ChatClickEvent("suggest_command", msg)
+final case class ChangePage(nPage: Int) extends ChatClickEvent("change_page", nPage.toString)
