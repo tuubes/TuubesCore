add6a27ddb563c9150e127f81c57dfaa55c1a439
TheElectronWill <theelectronwill@gmail.com>
2017-12-30 23:35:40 +0100

    Associate an ID to each actor
    Each GameObject need an ID for the server/client communication, but each other Actor also needs an ID for internode communication (distributed computing - will be fully implemented later).

--- core/src/main/scala/org/tuubes/core/engine/GameObjectId.scala
+++
@@ -1,8 +0,0 @@
-package org.tuubes.core.engine
-
-/**
- * A unique ID associated to a [[GameObject]].
- *
- * @author TheElectronWill
- */
-private[tuubes] final class GameObjectId(val id: Int) extends AnyVal {}
