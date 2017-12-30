90eaca04eabc0f0151a567d49242cc9a224b9581
TheElectronWill <theelectronwill@gmail.com>
2017-12-30 23:38:46 +0100

    Work on the ExecutionGroup

--- core/src/main/scala/org/tuubes/core/engine/ExecutionGroup.scala
+++ core/src/main/scala/org/tuubes/core/engine/ExecutionGroup.scala
@@ -1,13 +1,23 @@
 package org.tuubes.core.engine
 
+import java.util.concurrent.ConcurrentLinkedQueue
+
 import com.electronwill.collections.SimpleBag
+import ActorState._
+
+import scala.util.control.NonFatal
 
 /**
+ * Processes a group of [[LocalActor]]s. An ExecutionGroup is intended to be scheduled as a task
+ * by an ExecutorService.
+ *
  * @author TheElectronWill
  */
 final class ExecutionGroup extends Runnable {
-	private val toUpdate = new SimpleBag[Updatable](256)
+	private val toUpdate = new SimpleBag[LocalActor](256)
 	private var lastTime: Double = Double.NaN
+
+	private val toAdd = new ConcurrentLinkedQueue[LocalActor]
 
 	override def run(): Unit = {
 		if (lastTime == Double.NaN) {
@@ -17,9 +27,30 @@ final class ExecutionGroup extends Runnable {
 			val dt = time - lastTime
 			lastTime = time
 
-			for (updatable <- toUpdate) {
-				updatable.update(dt)
+			val it = toUpdate.iterator
+			while (it.hasNext) {
+				val actor = it.next()
+				try {
+					actor.processMessages()
+					actor.state match {
+						case Terminated =>
+							it.remove()
+						case Moving =>
+							it.remove()
+							actor.group += actor
+					}
+					actor.update(dt)
+				} catch {
+					case NonFatal(e) =>
+					// TODO log
+				}
 			}
 		}
+	}
+
+	def +=(actor: LocalActor): Unit = {
+		assert(actor.state != Running && actor.group == this)
+		toAdd.offer(actor)
+		actor.state = ActorState.Running
 	}
 }
