15fc31f0cf585c60b4ee362d0b950b3596619474
TheElectronWill <theelectronwill@gmail.com>
2018-01-27 19:26:16 +0100

    Begin trait Protocol

---
+++ core/src/main/scala/org/tuubes/core/network/Protocol.scala
@@ -0,0 +1,36 @@
+package org.tuubes.core.network
+
+import com.electronwill.utils.IntBijection
+import org.tuubes.core.blocks.BlockType
+import org.tuubes.core.engine.GameObject
+import org.tuubes.core.entities.EntityType
+import org.tuubes.core.items.ItemType
+
+/**
+ * A network protocol used by the server to communicate with one client.
+ *
+ * @author TheElectronWill
+ */
+trait Protocol {
+	def spawn(obj: GameObject): Unit
+	def despawn(obj: GameObject): Unit
+	//TODO more methods
+
+	/**
+	 * A bijection of block ids.
+	 * [internal -> external] (direct) and [external -> internal] (inverse).
+	 */
+	def blockFunction: IntBijection[BlockType]
+
+	/**
+	 * A bijection of item ids.
+	 * [internal -> external] (direct) and [external -> internal] (inverse).
+	 */
+	def itemFunction: IntBijection[ItemType]
+
+	/**
+	 * A bijection of entity ids.
+	 * [internal -> external] (direct) and [external -> internal] (inverse).
+	 */
+	def entityFunction: IntBijection[EntityType]
+}
