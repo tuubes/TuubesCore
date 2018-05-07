6759ff8ca536744c4410d574852828486af4b5cb
Guillaume R <theelectronwill@gmail.com>
2018-05-07 17:00:17 +0200

    Complete and document the NetworkSystem object

--- core/src/main/scala/org/tuubes/core/network/NetworkSystem.scala
+++ core/src/main/scala/org/tuubes/core/network/NetworkSystem.scala
@@ -3,6 +3,7 @@ package org.tuubes.core.network
 import com.electronwill.niol.network.tcp.ScalableSelector
 import org.tuubes.core.TuubesServer.logger
 
+/** Object for network bases, manages the TCP Selector */
 object NetworkSystem {
   private val errorHandler = (e: Exception) => {
     logger.error("Error in the TCP ScalableSelector", e)
@@ -13,5 +14,19 @@ object NetworkSystem {
   private val stopHandler = () => {
     logger.info("TCP ScalableSelector stopped")
   }
+
+  /** The server's ScalableSelector that handles the TCP connections on several ports */
   val selector = new ScalableSelector(errorHandler, startHandler, stopHandler)
+
+  /** Starts the network system */
+  def start() = {
+    logger.info("Starting the network system...")
+    selector.start("Tuubes:Niol-TCP")
+  }
+
+  /** Stops the network system */
+  def stop() = {
+    logger.info("Stopping the network system...")
+    selector.stop()
+  }
 }
