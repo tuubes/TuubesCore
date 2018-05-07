5af0ce407c276f3603a28396cd4f0b268b87fadb
Guillaume R <theelectronwill@gmail.com>
2018-05-07 16:24:14 +0200

    Use null fCancel instead of null key to represent an invalid ListenRegistration

--- core/src/main/scala/org/tuubes/core/engine/ListenRegistration.scala
+++ core/src/main/scala/org/tuubes/core/engine/ListenRegistration.scala
@@ -5,9 +5,12 @@ package org.tuubes.core.engine
  *
  * @author TheElectronWill
  */
-final class ListenRegistration[A](private[this] var key: ListenKey[A],
-								  private[this] val fCancel: ListenKey[A] => Unit)
+final class ListenRegistration[A](private[this] val key: ListenKey[A],
+								  private[this] var fCancel: ListenKey[A] => Unit)
 	extends Registration[A] {
-	override def cancel(): Unit = fCancel(key)
-	override def isValid: Boolean = (key != null)
+	override def cancel(): Unit = {
+		fCancel(key)
+		fCancel = null
+	}
+	override def isValid: Boolean = (fCancel != null)
 }
