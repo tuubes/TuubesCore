42e51ea058436a4d9a7d214b4b021ac4b224d89d
Guillaume R <theelectronwill@gmail.com>
2018-05-09 02:56:22 +0200

    Implement WorldPluginManager class

--- core/src/main/scala/org/tuubes/core/plugins/WorldPluginManager.scala
+++ core/src/main/scala/org/tuubes/core/plugins/WorldPluginManager.scala
@@ -1,7 +1,31 @@
 package org.tuubes.core.plugins
 
-trait WorldPluginManager {
-	def enable(plugin: Plugin)
+import org.tuubes.core.engine.World
 
-	def disable(plugin: Plugin)
+import scala.collection.mutable
+
+/**
+ * Manages the plugins of a specific world.
+ *
+ * @param world the managed world
+ */
+class WorldPluginManager(val world: World) {
+	protected val enabled = new mutable.AnyRefMap[String, Plugin]()
+
+	def plugins: Iterable[Plugin] = enabled.values
+
+	def plugin(name: String): Option[Plugin] = enabled.get(name)
+
+	def enable(p: Plugin): Unit = {
+		if (p.state != PluginState.DISABLED && !enabled.contains(p.name)) {
+			p.onEnable(world)
+			enabled(p.name) = p
+		}
+	}
+
+	def disable(p: Plugin): Unit = {
+		if (p.state != PluginState.DISABLED && enabled.remove(p.name).isDefined) {
+			p.onDisable(world)
+		}
+	}
 }
