3e33e045e942a889a8362a3fe4dcca06cf611795
Guillaume R <theelectronwill@gmail.com>
2018-05-09 02:59:31 +0200

    Main: ansi + plugins + network

--- core/src/main/scala/org/tuubes/core/TuubesServer.scala
+++ core/src/main/scala/org/tuubes/core/TuubesServer.scala
@@ -1,7 +1,10 @@
 package org.tuubes.core
 
 import better.files.File
+import org.fusesource.jansi.AnsiConsole
 import org.slf4j.LoggerFactory
+import org.tuubes.core.network.NetworkSystem
+import org.tuubes.core.plugins.ScalaPluginLoader
 
 /**
  * @author TheElectronWill
@@ -17,7 +20,23 @@ object TuubesServer {
 	val DirWorlds: File = DirMain / "worlds"
 	val DirLogs: File = DirMain / "logs"
 
-	def main(args: Array[String]): Unit = {
+	val PluginLoader = new ScalaPluginLoader
 
+	def main(args: Array[String]): Unit = {
+		logger.info("Tuubes core loading...")
+		logger.warn("WARNING: This is an unreleased version of TuubesCore, potentially unstable")
+
+		AnsiConsole.systemInstall()
+		// TODO read configuration(s?)
+
+		logger.info("Let's load the plugins!")
+		PluginLoader.load(DirPlugins.list)
+
+		// TODO load the world(s)
+		logger.info("Connecting to the world...")
+		NetworkSystem.start()
+
+		// TODO add shutdown hooks
+		logger.info(s"Tuubes $Version is ready!")
 	}
 }
