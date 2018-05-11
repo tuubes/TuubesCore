4c909773ad6a95cbf7f014f940fef51d93a6495e
Guillaume R <theelectronwill@gmail.com>
2018-05-11 21:49:16 +0200

    Network: more documentation

--- core/src/main/scala/org/tuubes/core/network/Packet.scala
+++ core/src/main/scala/org/tuubes/core/network/Packet.scala
@@ -4,7 +4,12 @@ import com.electronwill.niol.NiolOutput
 
 /** A network packet that can be written */
 trait Packet {
+  /** The packet's id in its protocol */
   final def id: Int = obj.id
+
+  /** The packet object (usually a companion object of the packet class) */
   def obj: PacketObj[this.type]
+
+  /** Writes this packet to the given output */
   def write(out: NiolOutput): Unit
 }
--- core/src/main/scala/org/tuubes/core/network/PacketObj.scala
+++ core/src/main/scala/org/tuubes/core/network/PacketObj.scala
@@ -4,6 +4,9 @@ import com.electronwill.niol.NiolInput
 
 /** Trait for packet companion objects */
 trait PacketObj[P <: Packet] {
+  /** The packet's id */
   def id: Int
+
+  /** Reads a packet of this type from the given input */
   def read(in: NiolInput): P
 }
