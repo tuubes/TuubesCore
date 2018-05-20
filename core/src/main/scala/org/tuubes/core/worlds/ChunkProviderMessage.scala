7557400c3112ebdb70989831adf32008fc074ec2
Guillaume R <theelectronwill@gmail.com>
2018-05-20 15:19:55 +0200

    Regroup ChunkService messages in one file

--- core/src/main/scala/org/tuubes/core/worlds/ChunkProviderMessage.scala
+++
@@ -1,8 +0,0 @@
-package org.tuubes.core.worlds
-
-import org.tuubes.core.engine.ActorMessage
-
-sealed trait ChunkProviderMessage extends ActorMessage
-final case class RequestCreate(cx: Int, cy: Int, cz: Int, callback: Chunk => ()) extends ChunkProviderMessage
-final case class RequestExisting(cx: Int, cy: Int, cz: Int, callback: Option[Chunk] => ()) extends ChunkProviderMessage
-final case class TestExists(cx: Int, cy: Int, cz: Int, callback: Boolean => ()) extends ChunkProviderMessage
---
+++ core/src/main/scala/org/tuubes/core/worlds/chunkServiceMessages.scala
@@ -0,0 +1,9 @@
+package org.tuubes.core.worlds
+
+import org.tuubes.core.engine.ActorMessage
+
+sealed trait ChunkServiceMessages extends ActorMessage
+final case class RequestCreate(cx: Int, cy: Int, cz: Int, callback: Chunk => ()) extends ChunkServiceMessages
+final case class RequestExisting(cx: Int, cy: Int, cz: Int, callback: Option[Chunk] => ()) extends ChunkServiceMessages
+final case class TestExists(cx: Int, cy: Int, cz: Int, callback: Boolean => ()) extends ChunkServiceMessages
+final case class ChunkLoaded(key: Long, chunk: Chunk) extends ChunkServiceMessages
