ae36d875e53fc37715aa5244d2683f3daac7cc65
Guillaume R <theelectronwill@gmail.com>
2018-06-21 15:17:04 +0200

    Basic actor ids with an atomic counter

--- core/src/main/scala/org/tuubes/core/engine/ActorId.scala
+++ core/src/main/scala/org/tuubes/core/engine/ActorId.scala
@@ -1,8 +1,15 @@
 package org.tuubes.core.engine
 
+import java.util.concurrent.atomic.AtomicInteger
+
 /**
  * A unique ID associated to an [[Actor]].
  *
  * @author TheElectronWill
  */
 private[tuubes] final class ActorId(val id: Int) extends AnyVal {}
+object ActorId {
+  private val counter = new AtomicInteger()
+
+  private[tuubes] def next(): ActorId = new ActorId(counter.getAndIncrement())
+}
