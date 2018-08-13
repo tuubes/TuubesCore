d19e0846ff0cb89cb2638217483e04fa285b574c
Guillaume R <theelectronwill@gmail.com>
2018-08-13 11:27:26 +0200

    Implement pre-registrations

--- core/src/org/tuubes/core/TypeRegistry.scala
+++ core/src/org/tuubes/core/TypeRegistry.scala
@@ -1,6 +1,8 @@
 package org.tuubes.core
 
-import com.electronwill.collections.RecyclingIndex
+import better.files.File
+import com.electronwill.collections.ArrayMap
+import com.electronwill.utils.StringUtils
 
 import scala.collection.mutable
 import scala.reflect.ClassTag
@@ -10,9 +12,10 @@ import scala.reflect.ClassTag
  *
  * @tparam T the type registered in this registry
  */
-class TypeRegistry[T >: Null <: Type[T]: ClassTag] {
-  private[this] val index = new RecyclingIndex[T]
-  private[this] val namesMap = new mutable.AnyRefMap[String, T]
+class TypeRegistry[T >: Null <: Type[T] : ClassTag] {
+  private[this] val index = new ArrayMap[T](64, null)
+  private[this] val namesMap = new mutable.AnyRefMap[String, T](64)
+  private[this] var preRegistrations = new mutable.AnyRefMap[String, Int](64)
 
   def get(uniqueName: String): Option[T] = {
     namesMap.get(uniqueName)
@@ -27,11 +30,21 @@ class TypeRegistry[T >: Null <: Type[T]: ClassTag] {
   }
 
   private[tuubes] def getOrNull(internalId: Int): T = {
-    index.getOrNull(internalId)
+    index(internalId)
   }
 
   private[tuubes] def register(t: T): Int = {
     namesMap(t.uniqueName) = t
-    index += t
+    val key = preRegistrations.getOrElse(t.uniqueName, index.size)
+    index(key) = t
+    key
+  }
+
+  private[tuubes] def preRegister(uniqueName: String, id: Int): Unit = {
+    preRegistrations(uniqueName) = id
+  }
+
+  private[tuubes] def ditchPreRegistrations(): Unit = {
+    preRegistrations = null
   }
 }
