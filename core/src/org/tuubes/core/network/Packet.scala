6949b2639ca036d8b8d9180d2fd7f859e6b4c842
Guillaume R <theelectronwill@gmail.com>
2018-08-23 18:02:26 +0200

    Use Niol's Writeable and Reader traits for the packets

--- core/src/org/tuubes/core/network/Packet.scala
+++ core/src/org/tuubes/core/network/Packet.scala
@@ -1,13 +1,9 @@
 package org.tuubes.core.network
 
-import com.electronwill.niol.NiolOutput
-import com.electronwill.niol.network.tcp.ClientAttach
+import com.electronwill.niol.Writeable
 
 /** A network packet that can be written */
-trait Packet {
+trait Packet extends Writeable {
   /** The packet's id in its protocol */
   def id: Int
-
-  /** Writes this packet to the given output */
-  def write(out: NiolOutput): Unit
 }
